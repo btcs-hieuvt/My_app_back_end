fc6045dd74e5594212ae3c625ec5daa3f5f56fd5
Vũ Trung Hiếu <hieuvt@btc-studio.io>
2022-10-09 17:06:47 +0700

    add package

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const express = require('express')
-const cors = require("cors")
-const app = express()
-const mongoose = require("mongoose")
-var bodyParser = require("body-parser")
-const morgan = require("morgan")
-const dotenv = require("dotenv")
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
 
-dotenv.config()
+dotenv.config();
 //CONNECT DB
-mongoose.connect((process.env.MONGODB_URL),()=>{
-    console.log("Connected to MongoDB");
-})
+mongoose.connect(process.env.MONGODB_URL, () => {
+  console.log("Connected to MongoDB");
+});
 
-app.use(bodyParser.json({limit:"50mb"}))
-app.use(cors())
-app.use(morgan("common"))
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(cors());
+app.use(morgan("common"));
 
-app.get("/" ,(req ,res)=>{
-    res.status(200).json("hello")
-})
+app.get("/", (req, res) => {
+  res.status(200).json("hello");
+});
 
-app.listen(8000,()=>{
-    console.log("server is running...");
-})
+app.listen(8000, () => {
+  console.log("server is running...");
+});
